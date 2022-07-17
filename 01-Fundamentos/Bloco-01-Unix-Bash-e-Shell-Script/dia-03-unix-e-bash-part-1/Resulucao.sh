abb235e0f1a0662c1c49fb613bd503aab824e5b7
gabrielmatina <109090997+gabrielmatina@users.noreply.github.com>
2022-07-17 00:48:39 -0300

    Resolvendo exercicio 9

--- 01-Fundamentos/Bloco-01-Unix-Bash-e-Shell-Script/dia-03-unix-e-bash-part-1/Resulucao.sh
+++ 01-Fundamentos/Bloco-01-Unix-Bash-e-Shell-Script/dia-03-unix-e-bash-part-1/Resulucao.sh
@@ -1,46 +1,53 @@
 # Exercício 1
 
 # Crie um diretório chamado unix_tests.
-mkdir unix_tests;
+mkdir unix_tests
 # Navegue até ele
-cd unix_tests;
+cd unix_tests
 
 
 # Exercício 2
 
 # Crie um arquivo de texto com o nome trybe.txt.
-touch trybe.txt;
+touch trybe.txt
 
 
 # Exercício 3
 
 # Crie uma cópia do arquivo trybe.txt com nome trybe_backup.txt.
-cp trybe.txt trybe_backup.txt;
+cp trybe.txt trybe_backup.txt
 
 
 # Exercício 4
 Renomeie o arquivo trybe.txt.
-mv trybe.txt trybe1.txt;
+mv trybe.txt trybe1.txt
 
 # Exercicio 5
 
 # Dentro de unix_tests, crie um novo diretório chamado backup.
-mkdir backup;
+mkdir backup
 
 
 # Exercicio 6
 
 # Mova o arquivo trybe_backup.txt para o diretório backup.
-mv trybe_backup.txt backup;
+mv trybe_backup.txt backup
 
 
-#Exercicio 7
+# Exercicio 7
 
-Dentro de unix_tests, crie um novo diretório chamado backup2.
-mkdir backup2;
+# Dentro de unix_tests, crie um novo diretório chamado backup2.
+mkdir backup2
 
 
 #Exercicio 8
 
-Mova o arquivo trybe_backup.txt da pasta backup para a pasta backup2.
-mv backup/trybe_backup.txt backup2;
+# Mova o arquivo trybe_backup.txt da pasta backup para a pasta backup2.
+mv backup/trybe_backup.txt backup2
+
+
+# Exercicio 9
+
+# Apague a pasta backup.
+rmdir backup
+
