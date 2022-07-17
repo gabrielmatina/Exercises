48bcf63424e894be2e431758ca5884a66eabffe3
gabrielmatina <109090997+gabrielmatina@users.noreply.github.com>
2022-07-17 14:20:32 -0300

    Resolvendo exercicio 2 e 3

--- 01-Fundamentos/Bloco-01-Unix-Bash-e-Shell-Script/dia-03-unix-e-bash-part-1/Resolucao-Parte-2.sh
+++ 01-Fundamentos/Bloco-01-Unix-Bash-e-Shell-Script/dia-03-unix-e-bash-part-1/Resolucao-Parte-2.sh
@@ -3,3 +3,16 @@
 # Na pasta unix_tests, baixe um arquivo com os nomes de todos os países do mundo utilizando o comando curl:
 curl -o countries.txt "https://gist.githubusercontent.com/kalinchernev/486393efcca01623b18d/raw/daa24c9fea66afb7d68f8d69f0c4b8eeb9406e83/countries"
 
+
+# Exercicio 2
+
+# Mostre todo o conteúdo do arquivo countries.txt na tela.
+cat countries.txt
+
+
+# Exercicio 3
+
+# Mostre o conteúdo de countries.txt, página por página, até encontrar a Zambia.
+cat countries.txt
+
+
