5a4b16d45ef9b62b81d6b1dc7e895ee83925a661
gabrielmatina <109090997+gabrielmatina@users.noreply.github.com>
2022-07-17 00:40:23 -0300

    Resolvendo exercicio 5

--- 01-Fundamentos/Bloco-01-Unix-Bash-e-Shell-Script/dia-03-unix-e-bash-part-1/Resulucao.sh
+++ 01-Fundamentos/Bloco-01-Unix-Bash-e-Shell-Script/dia-03-unix-e-bash-part-1/Resulucao.sh
@@ -20,6 +20,9 @@ cp trybe.txt trybe_backup.txt;
 
 # Exercício 4
 Renomeie o arquivo trybe.txt.
-mv trybe.txt trybe1.txt
+mv trybe.txt trybe1.txt;
 
+# Exercicio 5
 
+# Dentro de unix_tests, crie um novo diretório chamado backup.
+mkdir backup;
